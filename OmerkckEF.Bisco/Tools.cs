336dfdcfc2b522619048b254121668806010f2d8
omerkck41 <554654amk>
2023-04-19 02:00:52 +0300

    last update with tools class

--- OmerkckEF.Bisco/Tools.cs
+++ OmerkckEF.Bisco/Tools.cs
@@ -29,42 +29,42 @@ namespace OmerkckEF.Biscom
 
             if (prop.PropertyType == typeof(string))
             {
-                prop.SetValue(entity, value.ToString().Trim(), null);
+                prop.SetValue(entity, ((string)value).Trim(), null);
             }
             else if (prop.PropertyType == typeof(char) || prop.PropertyType == typeof(char?))
             {
                 if (value == null)
                     prop.SetValue(entity, null, null);
                 else
-                    prop.SetValue(entity, char.Parse(value.ToString()), null);
+                    prop.SetValue(entity, char.Parse((string)value), null);
             }
             else if (prop.PropertyType == typeof(int) || prop.PropertyType == typeof(int?))
             {
                 if (value == null)
                     prop.SetValue(entity, null, null);
                 else
-                    prop.SetValue(entity, int.Parse(value.ToString()), null);
+                    prop.SetValue(entity, int.Parse((string)value), null);
             }
             else if (prop.PropertyType == typeof(bool) || prop.PropertyType == typeof(bool?))
             {
                 if (value == null)
                     prop.SetValue(entity, null, null);
                 else
-                    prop.SetValue(entity, bool.Parse(value.ToString()), null);
+                    prop.SetValue(entity, bool.Parse((string)value), null);
             }
             else if (prop.PropertyType == typeof(DateTime) || prop.PropertyType == typeof(DateTime?))
             {
                 if (value == null)
                     prop.SetValue(entity, null, null);
                 else
-                    prop.SetValue(entity, DateTime.Parse(value.ToString()), null);
+                    prop.SetValue(entity, DateTime.Parse((string)value), null);
             }
             else if (prop.PropertyType == typeof(decimal) || prop.PropertyType == typeof(decimal?))
             {
                 if (value == null)
                     prop.SetValue(entity, null, null);
                 else
-                    prop.SetValue(entity, decimal.Parse(value.ToString()), null);
+                    prop.SetValue(entity, decimal.Parse((string)value), null);
             }
             else if (prop.PropertyType == typeof(byte[]))
             {
